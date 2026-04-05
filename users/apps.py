e5901b36513adfad1dec9d886ecbb81e6f537692
josh-dimpas <jk.dimpas@academy.opswerks.com>
2026-04-06 03:00:55 +0800

    fix: added default jwt_secret to appease types
    Should error still if no jwt_secret found in env

--- users/apps.py
+++ users/apps.py
@@ -6,5 +6,5 @@ class UsersConfig(AppConfig):
     name = 'users'
 
     JWT_LIFETIME = int(os.getenv('JWT_LIFETIME_SECONDS', '7200'))
-    JWT_KEY = os.getenv('JWT_SECRET')
+    JWT_KEY = os.getenv('JWT_SECRET', "")
 
