a875e669563ef163faa1772fa46b0eebacc9221e
josh-dimpas <jk.dimpas@academy.opswerks.com>
2026-04-06 02:36:19 +0800

    feat: use drf-simplejwt for extracing user id from request object

--- game/utils.py
+++ game/utils.py
@@ -1,9 +1,8 @@
-from django.conf import settings
-from django.contrib.auth.models import User
-from jwt import ( JWT)
+from typing import Optional
 
-import random
-import string
+from django.apps import apps
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 from django.http import HttpRequest
 from game.apps import GameConfig
@@ -11,27 +10,26 @@ from users.models import Account
 
 config: GameConfig = apps.get_app_config('game')
 
-def generate_code():
-    characters = string.ascii_letters + string.digits
-
-    # Continuously create code until a unique one appears 
-    while True:
-        code = random.choice(string.ascii_letters) + "".join(random.choices(characters, k=config.GAME_CODE_LENGTH))
-        if not Game.objects.filter(code=code).exists():
-            return code
-
-def get_user(request: HttpRequest):
-    auth_header = request.headers.get('Authorization', '')
-
-    # For Anon
-    if request.path.startswith(f'/{config.ANON_USERNAME}'):
+def get_user_from_request(request: HttpRequest) -> Optional[int]:
+    if not hasattr(request, "META"):
         return None
 
+    # Check for anon
+    path_username = getattr(request, "path_username", None)
+    if path_username == config.ANON_USERNAME:
+        return None
+
+    # Decode JWT
+    auth_header = request.META.get('HTTP_AUTHORIZATION', "")
+    if not auth_header.startswith("Bearer "):
+        return None
+        
     token = auth_header[7:]
 
     try:
-        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
-        return User.objects.get(username=payload['username'])
-    except BaseException as e:
-        print(e)
-        pass
+        access_token = AccessToken(token)
+        user_id = access_token['user_id']
+        account = Account.objects.get(id=user_id)
+        return account.username
+    except (InvalidToken, TokenError, Account.DoesNotExist) as e:
+        return None
