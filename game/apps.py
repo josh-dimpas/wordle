d5f6cadd57be4a00b78565f6268aeacce1bd1052
josh-dimpas <jk.dimpas@academy.opswerks.com>
2026-04-06 06:26:35 +0800

    style: changed indentation (prettier)

--- game/apps.py
+++ game/apps.py
@@ -3,15 +3,15 @@ from typing import Final
 
 import os
 
+
 class GameConfig(AppConfig):
-    name = 'game'
+    name = "game"
 
-    GAME_CODE_LENGTH: Final = int(os.getenv('GAME_CODE_LENGTH', '6'))
-    GAME_MAX_TRIES: Final = int(os.getenv('GAME_MAX_TRIES', '6'))
+    GAME_MAX_TRIES: Final = int(os.getenv("GAME_MAX_TRIES", "6"))
 
-    WORD_MAX_LENGTH: Final = int(os.getenv('WORD_MAX_LENGTH', '5'))
-    WORD_MIN_LENGTH: Final = int(os.getenv('WORD_MIN_LENGTH', '5'))
-    WORD_CORRECT_EXACT_IDENTIFIER: Final = os.getenv('WORD_CHAR_EXACT_IDENTIFIER', '/')
-    WORD_CORRECT_PARTIAL_IDENTIFIER: Final = os.getenv('WORD_CHAR_PARTIAL_IDENTIFIER', '+')
-
-    ANON_USERNAME: Final = os.getenv('ANON_USERNAME', 'anon')
+    WORD_MAX_LENGTH: Final = int(os.getenv("WORD_MAX_LENGTH", "5"))
+    WORD_MIN_LENGTH: Final = int(os.getenv("WORD_MIN_LENGTH", "5"))
+    WORD_CORRECT_EXACT_IDENTIFIER: Final = os.getenv("WORD_CHAR_EXACT_IDENTIFIER", "/")
+    WORD_CORRECT_PARTIAL_IDENTIFIER: Final = os.getenv(
+        "WORD_CHAR_PARTIAL_IDENTIFIER", "+"
+    )
